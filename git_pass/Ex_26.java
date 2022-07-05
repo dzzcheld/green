de48acf590ed5a6632a046ea5f2e7d4d773e2664
dzzcheld <34300575+dzzcheld@users.noreply.github.com>
2022-07-05 19:28:02 +0900

    Add files via upload
    배열 크기비교

--- git_pass/Ex_26.java
+++ git_pass/Ex_26.java
@@ -1,58 +1,58 @@
-package get_0701;
+package get_0630;
+
+import java.util.Random;
+import java.util.*;
 
 public class Ex_26 {
 
 	public static void main(String[] args) {
-		int []num= {23, 45, 13, 50, 25};
-		int tmp=0;
 		
-		for(int i=0; i<5; i++) {
-			if(num[i+1]>num[i]) {
+		
+		
+		
+		
+		
+		int[] num = { 45, 23, 13, 50, 25 };
+		int blank = 0;
+		int i = 0;
+
+		for (int k = 1; k < num.length - 1; k++) {
+
+			for (i = 1; i < num.length - 1; i++) {
+				if (num[i - 1] > num[i]) {
+					blank = num[i];
+					num[i] = num[i - 1];
+					num[i - 1] = blank;
+					continue;
+				}
+				blank = num[num.length - k];
+				num[num.length - k] = num[num.length - k - 1];
+				num[num.length - k - 1] = blank;
 				continue;
-			}else{
-				tmp=num[i];
-				num[i]=num[i+1];
-				num[i+1]=tmp;
-				
+
 			}
-			
-			for(i=0; i<5; i++)
-				System.out.println("결과는" + num[i]);
-	
 		}
-	
-	
+		for (int k = 0; k < num.length; k++) {
+			System.out.println(num[k]);
+		}
+
 	}
+
 }
-	
-
 /*
- * i=0일 떄
- * 1인덱스가 0인덱스 보다 크기에 유지
+ * Random.ran=new Random();
+ * int num[] =(int)(ran*50)+1;
  * 
- * i=1 일 떄
- * 인덱스 1이 인덱스 2보다 크기에 45가 2에 들어감
- * 
- * i=2 일 떄
- * 인덱스 3이(50) 2보다(45) 크기에 유지
- * 
- * =>모든 요소와 비교하지 않으며 가장 큰 수가 멈춘다.
+ * =============================================================================
+ * ================== [문제26] 크기가 5인 정수형배열을 잡아 난수(1~50)를 저장하여 출력하시오
  * 
  * 
- *===================================================================
- *[문제26] 크기가 5인 정수형배열을 잡아 난수(1~50)를 저장하여 출력하시오
-
-
---출력--
-
-[정렬전]
-num[0]=23  num[1]=45  num[2]=13  num[3]=50  num[4]=25
-
-[정렬후]
-num[0]=13  num[1]=23  num[2]=25  num[3]=45  num[4]=50
-
-합 = xx
-출처: https://private.tistory.com/13 [오토봇팩토리:티스토리] 
+ * --출력--
  * 
- *
+ * [정렬전] num[0]=23  num[1]=45  num[2]=13  num[3]=50  num[4]=25
+ * 
+ * [정렬후] num[0]=13  num[1]=23  num[2]=25  num[3]=45  num[4]=50
+ * 
+ * 합 = xx 출처: https://private.tistory.com/13 [오토봇팩토리:티스토리]
+ * 
  */
